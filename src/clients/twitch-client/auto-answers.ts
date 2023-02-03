e43810d9c7c088f7ca2790ff2ca1e5004c186506
Alexander Melnikov <okman5757@gmail.com>
2023-02-04 00:33:55 +0300

    add new answer

--- src/clients/twitch-client/auto-answers.ts
+++ src/clients/twitch-client/auto-answers.ts
@@ -99,6 +99,7 @@ export const autoAnswers: {
     "лера лох": (options) => `@${options.tags["display-name"]} соглы`,
     "марк лох": (options) => `@${options.tags["display-name"]} соглы`,
     "коля лох": (options) => `@${options.tags["display-name"]} соглы`,
+    "сколько см у карины": (opitons) => `@${opitons.tags["display-name"]} у карины ${randomInteger(3, 40)} см`,
     "сколько см у юли": (opitons) => `@${opitons.tags["display-name"]} у юли ${randomInteger(3, 40)} см`,
     "сколько у меня см": (options) => `@${options.tags["display-name"]} у тебя ${randomInteger(0, 40)} см`,
     "привет саня": () => "прив",
